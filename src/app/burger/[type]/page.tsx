892f82ff6af05b753fdabf061ef28545f61c8d96
John Pham <johnphammail@gmail.com>
2023-10-27 17:58:33 -0700

    fix bug

--- src/app/burger/[type]/page.tsx
+++ src/app/burger/[type]/page.tsx
@@ -141,7 +141,7 @@ const Component = ({ type }: { type: string }) => {
             type="submit"
             className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
           >
-            Submit Order to Grill Master
+            Submit Order to Grill Master (click me twice)
           </button>
         </div>
       </form>
